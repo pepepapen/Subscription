662879b4e0d6b6e35c9385d5f1512f7b7b204b0a
pepepapen <105201126+pepepapen@users.noreply.github.com>
2022-05-23 15:12:46 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0571ix/subscription/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.ritsumei.ise.phy.exp2.is0571ix.subscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
